0d033f57fda2eb7776c29e1d056632c6ddd0c420
DariaSemenova21 <99136467+DariaSemenova21@users.noreply.github.com>
2022-06-01 20:33:12 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Salon.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Salon
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Role? CurrentRole = null;
+        public static Object Human = null;
+
+        public static Customer GetCustomer() => Human as Customer;
+        public static Employee GetEmployee() => Human as Employee;
+
+
+        public static MessageBoxResult ShowMessage(String message, String title = "Сообщение", MessageBoxButton button  = MessageBoxButton.OK, MessageBoxImage image = MessageBoxImage.Asterisk)
+        {
+            return MessageBox.Show(message, title, button, image );
+        }
+    }
+}
