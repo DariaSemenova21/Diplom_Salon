2281ddf9649a27b5f872ff5beb96044576f0edb4
DariaSemenova21 <99136467+DariaSemenova21@users.noreply.github.com>
2022-06-01 20:37:37 +0300

    Add files via upload

---
+++ Models/PartialClass/partialWorks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.IO;
+
+namespace Salon.Models
+{
+    public partial class Works
+    {
+        public byte[] Works1
+        {
+            get
+            {
+                if (works == null || works.Length == 0) return new Byte[] { };
+
+                if (File.Exists($"{works.Substring(1)}"))
+                {
+                    return File.ReadAllBytes($"{works.Substring(1)}");
+                }
+                if (File.Exists($"masters/picture.png"))
+                {
+                    return File.ReadAllBytes("masters\\picture.png");
+                }
+                return null;
+            }
+        }
+
+        public Double height;
+        public double Width
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+    }
+}
---
+++ Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon.Models
+{
+    public enum Role
+    {
+        Master = 1,
+        Admin = 2,
+        Customer = 3
+    }
+}
