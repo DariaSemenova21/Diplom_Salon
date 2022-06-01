84e7fa7f88f69a087ce1ab6010ebe691cd0535f3
DariaSemenova21 <99136467+DariaSemenova21@users.noreply.github.com>
2022-06-01 20:41:31 +0300

    Add files via upload

---
+++ Tools/DataBase.cs
@@ -0,0 +1,23 @@
+﻿using Salon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon.Tools
+{
+   public static class DataBase
+    {
+        public static void Query(SalonEntities1 db, Action<SalonEntities1> action)
+        {
+            db = new SalonEntities1();
+            action(db);
+        }
+        public static T Query<T>(SalonEntities1 db, Func<SalonEntities1, T> action)
+        {
+            db = new SalonEntities1();
+           return action(db);
+        }
+    }
+}
